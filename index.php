ad7407930275d338cdef8707b9b666667c531f8c
egananda <egagithub66@gmail.com>
2022-01-28 21:36:32 +0700

    mengubah isi file index.php

--- index.php
+++ index.php
@@ -10,7 +10,10 @@
 
 <body>
     <h1>Coba test git 2</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla illum distinctio sequi ex repellat, quos quas fugiat accusamus alias velit deleniti, quasi dolorem doloribus nostrum eveniet esse! Pariatur explicabo obcaecati in doloremque molestias consequuntur debitis veritatis? Earum beatae tenetur possimus, nobis atque similique nulla reiciendis molestiae pariatur ipsa. Facilis cumque voluptas quia impedit, nemo veniam ea quas nam consequuntur ipsa voluptates aliquam illum temporibus, doloremque unde possimus accusantium iure voluptatum sed! Molestiae corporis velit odio unde dolorem quis iste? Cumque quas soluta, nihil maxime, sunt atque, voluptatem harum necessitatibus temporibus vitae sed neque beatae aperiam iusto quaerat sit ab perspiciatis doloremque excepturi! Cumque quis vero aperiam iste, numquam qui amet reprehenderit consequatur blanditiis dolor totam in incidunt commodi deleniti, impedit sequi enim mollitia officiis nulla laborum, saepe perspiciatis natus architecto? Corrupti nihil praesentium dolorum, esse quasi, beatae incidunt ullam optio dolor voluptatibus tenetur natus voluptatum nam reiciendis. Officiis inventore pariatur magnam, ducimus quas nam voluptate doloribus, distinctio aperiam ratione voluptatum tempora, illo molestiae nemo itaque eligendi! Laborum aliquam saepe molestias rem, architecto, obcaecati asperiores tempora consequuntur dolorum in illum praesentium sequi veritatis sapiente maxime quaerat animi, perferendis explicabo possimus placeat tempore? Autem voluptatum esse necessitatibus nisi ut nihil labore laborum?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla illum distinctio sequi ex repellat, quos quas fugiat accusamus alias velit deleniti, quasi dolorem doloribus nostrum eveniet esse! Pariatur explicabo obcaecati in doloremque molestias consequuntur debitis veritatis? Earum beatae tenetur possimus, nobis atque similique nulla reiciendis molestiae pariatur ipsa. Facilis cumque voluptas quia impedit, nemo veniam ea quas nam consequuntur ipsa voluptates aliquam illum temporibus, doloremque unde possimus accusantium</p>
+    <ul>
+        <li>Ini adalah list</li>
+    </ul>
 
     <a href="https://google.com">klik disini</a>
 </body>
